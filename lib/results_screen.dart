c1f96ac1bab8d33808db74e64f53fcd5a81a4d0b
Soosan Shabnam <soosan2609@gmail.com>
2023-08-04 15:58:56 +0530

    changes

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,6 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/data/questions.dart';
 class ResultsScreen extends StatelessWidget {
-  const ResultsScreen({super.key});
+  const ResultsScreen({
+    super.key,
+    required this.chosenAnswers,
+  });
+
+  final List<String> chosenAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add(
+        {
+          'question_index': i,
+          'question': questions[i].text,
+          'correct_answer': questions[i].answers[0],
+          'user_answer': chosenAnswers[i]
+        },
+      );
+    }
+
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,4 +51,5 @@ class ResultsScreen extends StatelessWidget {
       ),
     );
   }
+
 }
